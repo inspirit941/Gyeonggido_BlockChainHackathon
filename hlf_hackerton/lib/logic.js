3c135ac676f8058a3d2e00b94585d892b3fa0a3a
Lee Donggeon <inspirit941@gmail.com>
2018-08-08 19:37:23 +0900

    CreateCivil, Petition Asset 항목 추가

--- hlf_hackerton/lib/logic.js
+++ hlf_hackerton/lib/logic.js
@@ -30,7 +30,7 @@ async function sampleTransaction(tx) {
     tx.asset.value = tx.newValue;
 
     // Get the asset registry for the asset.
-    const assetRegistry = await getAssetRegistry('org.petition.prov.participants.SampleAsset');
+    const assetRegistry = await getAssetRegistry('org.petition.prov.participants.SampleAsset'); 
     // Update the asset in the asset registry.
     await assetRegistry.update(tx.asset);
 
@@ -40,6 +40,32 @@ async function sampleTransaction(tx) {
     event.oldValue = oldValue;
     event.newValue = tx.newValue;
     emit(event);
+}
+/**
+ * CreatePetition
+ * @param {org.petition.prov.participants.CreateCivil} CivilData
+ * @transaction
+ */
+function    CreateCivil(CivilData) {
+    return getParticipantRegistry('org.petition.prov.participants.Civil')
+        .then(function(CivilRegistry){
+            var factory = getFactory();
+            var NS =  'org.petition.prov.participants';
+
+            
+            var Civil = factory.newResource(NS,'Civil',CivilData.CivilId);
+            
+            Civil.CivilId = CivilData.CivilId;
+            Civil.CivilDetail = CivilData.CivilDetail;
+
+            // 이벤트 발생 필요함.
+            // var event = factory.newEvent(NS, 'FlightCreated');
+            // event.flightId = flightId;
+            // emit(event);
+
+            // 4. Add to registry
+            return CivilRegistry.add(Civil);
+        });
 }
 
 
@@ -65,26 +91,34 @@ function    createPetition(PetitionData) {
     // return getAssetRegistry('org.acme.airline.flight.Flight')
     return getAssetRegistry('org.petition.prov.petition.Petition')
         
-        .then(function(PetitionRegistry){ //flightRegistry가 여깄었음
+        .then(async function(PetitionRegistry){ //flightRegistry가 여깄었음
             // Now add the Flight - global function getFactory() called
-            var  factory = getFactory();
+            var factory = getFactory();
 
             var  NS =  'org.petition.prov.petition'; // Namespace
-
-            // Solution to exercise - Removed hardcoded value & invoked
-            // generate the flight ID
-            // 2.1 Set the flightNumber, flightId ... 
-            // var  flightId = generateFlightId(flightData.flightNumber,flightData.schedule);
-            // var  flight = factory.newResource(NS,'Flight',flightId);
-            var PetitionId = '1'; // 민원 일련번호를 만드는 방법이 필요함. 그냥 +1만 주구장창 할 수도 있고, 랜덤값 넣을 수도 있고...
-            var  petition = factory.newResource(NS,'Petition',PetitionId);
+            var NS_participant = 'org.petition.prov.participants.Civil'
             
-            petition.PetitionId = PetitionData.PetitionId;
-            petition.CivilId = PetitionData.CivilId;
+            // 현재 필요한 것.
+            // 1. participant에 정의된 Civil 본인만이 자기 civilid로 민원을 실행할 수 있어야 함.
+            // 네트워크에서 접속하면 본인의 card로 접속할 테니... 그걸 구분하는 방법이 필요한데.
+
+            // var currentCivil = getFullyQualifiedIdentifier(); 왜 인식을 못하지..
+            // if (currentCivil !== PetitionData.Civil){
+            //     throw new Error("Not same Civil.")
+            // }
+            
+            var timeNow = new Date().getTime()
+            var PetitionId = String(PetitionData.Civil.CivilId)+'-'+String(timeNow); 
+            // 일단 민원 고유번호를 시민번호 + 시간으로 정함.
+            var petition = factory.newResource(NS,'Petition',PetitionId);
+            
+            petition.PetitionId = PetitionId;
+            petition.Civil = PetitionData.Civil;
             petition.Content = PetitionData.Content;
             petition.title = PetitionData.title;
             
-
+            
+        
             // // Flight asset has an instance of the concept
             // // 2.2 Use the factory to create an instance of concept
             // var route = factory.newConcept(NS,"Route");
@@ -107,3 +141,5 @@ function    createPetition(PetitionData) {
             return PetitionRegistry.add(petition);
         });
 }
+
+        
