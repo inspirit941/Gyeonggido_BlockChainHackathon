0b1cc9fa97b005bb5f791211666ac10093299be7
Lee Donggeon <inspirit941@gmail.com>
2018-08-19 17:24:42 +0900

    for test

--- hlf_hackerton/lib/logic.js
+++ hlf_hackerton/lib/logic.js
@@ -17,30 +17,6 @@
  * Write your transction processor functions here
  */
 
-// /**
-//  * Sample transaction
-//  * @param {org.petition.prov.participants.SampleTransaction} sampleTransaction
-//  * @transaction
-//  */
-// async function sampleTransaction(tx) {
-//     // Save the old value of the asset.
-//     const oldValue = tx.asset.value;
-
-//     // Update the asset with the new value.
-//     tx.asset.value = tx.newValue;
-
-//     // Get the asset registry for the asset.
-//     const assetRegistry = await getAssetRegistry('org.petition.prov.participants.SampleAsset'); 
-//     // Update the asset in the asset registry.
-//     await assetRegistry.update(tx.asset);
-
-//     // Emit an event for the modified asset.
-//     let event = getFactory().newEvent('org.petition.prov.participants', 'SampleEvent');
-//     event.asset = tx.asset;
-//     event.oldValue = oldValue;
-//     event.newValue = tx.newValue;
-//     emit(event);
-// }
 /**
  * CreatePetition
  * @param {org.petition.prov.participants.CreateCivil} CivilData
